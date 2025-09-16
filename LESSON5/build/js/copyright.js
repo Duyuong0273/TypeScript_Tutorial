af669738b149490f81ae9e0ee09fb2a7a802356c
Duyuong0273 <duyuong0273@gmail.com>
2025-09-16 23:41:49 +0700

    learning assertions

---
+++ LESSON5/build/js/copyright.js
@@ -0,0 +1,19 @@
+"use strict";
+//Original JS Code
+// const year = document.getElementById("year")
+// const thisYear = new Date().getFullYear
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
+// 1str variation
+// let year: HTMLElement | null = document.getElementById("year")
+// let thisYear: string
+// thisYear = new Date().getFullYear.toString()
+// if(year){
+//     year.setAttribute("datetime", thisYear)
+//     year.textContent = thisYear
+// }
+// 2nd variation
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear.toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
